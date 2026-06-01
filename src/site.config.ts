e4d96a823a36d2b95901e750b46ed50176ef491f
Andrew Heins <andrew.heins@gmail.com>
2026-06-01 10:34:29 -0400

    Fixes homepage title

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   authorFullName: 'Andrew Heins',
-  professionalHeadline: 'President, Cantilever',
+  professionalHeadline: 'Personal Homepage',
   authorBio: "I'm President of Cantilever, a digital agency. I've been on the internet since MUDs and Geocities, and I'm still trying to figure out what we built. I still think we can deliver on the promise we saw in it as kids.",
   employerName: 'Cantilever',
   employerUrl: 'https://cantilever.co',
